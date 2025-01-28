38aec9d1f364e7c35f334ebecd91a5cc335ed040
zhaixianyu <1141927681@qq.com>
2025-01-28 15:42:31 +0800

    1

--- src/main/java/com/zxy/wuhuclient/Utils/PinYinSearch.java
+++ src/main/java/com/zxy/wuhuclient/Utils/PinYinSearch.java
@@ -9,14 +9,13 @@ import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombi
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 
 public class PinYinSearch {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         getPinYin("曾0长0");
     }
-    static ArrayList<String[]> pinyin = new ArrayList<>();
-    public static ArrayList<String> getPinYin(String str){
+
+    public static ArrayList<String> getPinYin(String str) {
 
         char[] ch = str.toCharArray();
         HanyuPinyinOutputFormat gs = new HanyuPinyinOutputFormat();
@@ -24,50 +23,52 @@ public class PinYinSearch {
         gs.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
         gs.setVCharType(HanyuPinyinVCharType.WITH_V);
 
+        ArrayList<String[]> pinyin = new ArrayList<>();
         try {
-            pinyin = new ArrayList<>();
             for (char c : ch) {
-                if(c<=128)pinyin.add(new String[]{""+c});
+                if (c <= 128) pinyin.add(new String[]{"" + c});
                 else pinyin.add(PinyinHelper.toHanyuPinyinStringArray(c, gs));
             }
         } catch (BadHanyuPinyinOutputFormatCombination e) {
             throw new RuntimeException(e);
         }
-        return getStrings();
+        return getStrings(pinyin);
     }
 
-    public static boolean hasPinYin(String zh,String py){
-        return getPinYin(zh).stream().anyMatch(s->s.contains(py));
+    public static boolean hasPinYin(String zh, String py) {
+        return getPinYin(zh).stream().anyMatch(s -> s.contains(py));
     }
+
     @NotNull
-    private static ArrayList<String> getStrings() {
+    private static ArrayList<String> getStrings(ArrayList<String[]> pinyin) {
+        //全拼
         ArrayList<String> pys1 = new ArrayList<>();
         ArrayList<String> pys2;
-
+        //首字母简拼
         ArrayList<String> pys3 = new ArrayList<>();
         ArrayList<String> pys4;
-
+        //如果输入的是 曾长 那么pinyin中的内容为 ["zeng","ceng"],["chang","zhang"]
         for (int i = 0; i < pinyin.size(); i++) {
             pys2 = new ArrayList<>();
             pys4 = new ArrayList<>();
-            for (int i1 = 0;i1 < pinyin.get(i).length && pinyin.get(i).length > 0; i1++) {
-                if(i==0) {
+            for (int i1 = 0; i1 < pinyin.get(i).length && pinyin.get(i).length > 0; i1++) {
+                if (i == 0) {
                     pys1.add(pinyin.get(i)[i1]);
-                    pys3.add(""+ pinyin.get(i)[i1].charAt(0));
-                }else {
+                    pys3.add("" + pinyin.get(i)[i1].charAt(0));
+                } else {
                     for (int i2 = 0; i2 < pys1.size(); i2++) {
                         pys2.add(pys1.get(i2) + pinyin.get(i)[i1]);
                         pys4.add(pys3.get(i2) + pinyin.get(i)[i1].charAt(0));
                     }
                 }
             }
-            if(i!=0) {
+            if (i != 0) {
                 pys1 = pys2;
                 pys3 = pys4;
             }
         }
         pys1.addAll(pys3);
-//        System.out.println(pys1);
         return pys1;
     }
+
 }
