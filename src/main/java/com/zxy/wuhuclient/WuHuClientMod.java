440f16d849ad8c4e9da61c5ee35c58a94be6e727
Cjsah <cjsah@cjsah.net>
2026-04-14 18:35:34 +0800

    register malilib config screen for mc 1.21+

--- src/main/java/com/zxy/wuhuclient/WuHuClientMod.java
+++ src/main/java/com/zxy/wuhuclient/WuHuClientMod.java
@@ -13,11 +13,17 @@ import net.minecraft.client.MinecraftClient;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+//#if MC > 12006
+import com.zxy.wuhuclient.config.ConfigUi;
+import fi.dy.masa.malilib.registry.Registry;
+import fi.dy.masa.malilib.util.data.ModInfo;
+//#endif
+
 public class WuHuClientMod implements ClientModInitializer, ModInitializer {
 
 	public static MinecraftClient client = MinecraftClient.getInstance();
 	public static final String MOD_ID = "wuhuclient";
-	public static final String MOD_NAME = "wuhuclient";
+	public static final String MOD_NAME = "WuHu Client";
 	public static final Logger LOGGER = LogManager.getLogger();
 	@Override
 	public void onInitialize() {
@@ -26,7 +32,11 @@ public class WuHuClientMod implements ClientModInitializer, ModInitializer {
 	void register(){
 		Configs.INSTANCE.load();
 		ConfigManager.getInstance().registerConfigHandler(MOD_ID, Configs.INSTANCE);
-
+		//#if MC > 12006
+		Registry.CONFIG_SCREEN.registerConfigScreenFactory(
+			new ModInfo(MOD_ID, MOD_NAME, ConfigUi::new)
+		);
+		//#endif
 		InputEventHandler.getKeybindManager().registerKeybindProvider(InputHandler.getInstance());
 		InputEventHandler.getInputManager().registerKeyboardInputHandler(InputHandler.getInstance());
 		HighlightBlockRenderer.init();
